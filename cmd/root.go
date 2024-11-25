bd7fd2efa8223c1d0af80c35aa150b0034b1d64a
Charles The Great <doobthegoober@gmail.com>
2024-11-24 16:00:30 -0800

    Update root.go
    add option to negate search of a string
    - useful when doing a dictionary attack on a login field

--- cmd/root.go
+++ cmd/root.go
@@ -17,6 +17,7 @@ func Execute() {
 	requestBody := flag.String("body", "", "specify POST request body (or file containing the body)")
 	headers := flag.String("H", "", "specify the file that contains headers [separated by line]")
 	search := flag.String("s", "", "specify a string to search for in response body 'Login Successful'")
+	noSearch := flag.Bool("N", false, "enable NOT searching for given string (used with search) [login failed]")
 	threads := flag.Int("t", 3, "specify thread count [default: 3]")
 	statusCode := flag.String("sc", "", "specify a status code(s) to output")
 	timeout := flag.Int("timeout", 5, "specify timeout in seconds [default 5]")
@@ -67,6 +68,8 @@ func Execute() {
 		contents, err := utils.ReadFile(*requestBody)
 		if err == nil { // get contents of file for body
 			body = strings.Join(contents, "\n")
+		} else {
+			body = *requestBody
 		}
 	}
 
@@ -77,6 +80,7 @@ func Execute() {
 		body,
 		fuzzList,
 		*search,
+		*noSearch,
 		*threads,
 		timeoutDuration,
 		statuscodeSplit)
